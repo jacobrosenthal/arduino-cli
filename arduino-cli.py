dbf84fc41d90af0fab4dda683b80085fa173a7d5
Enkel <karonth@gmail.com>
2016-05-28 23:41:11 +0200

    Fixed output format in console, added compilation time

--- arduino-cli.py
+++ arduino-cli.py
@@ -1,8 +1,13 @@
-import subprocess, sublime, sublime_plugin
+import subprocess, sublime, sublime_plugin, time
 
 def get_setting(name):
     settings = sublime.load_settings('arduino-cli.sublime-settings')
     return sublime.active_window().active_view().settings().get(name, settings.get(name))
+#removes the b'....' from the output string and fixes the newlines.
+def clean_output(arg):
+    output = str(arg)
+    output = output[2:-1].replace("\\n","\n")
+    return output
 
 class ArduinoCommand(sublime_plugin.WindowCommand):
 
@@ -28,13 +33,22 @@ class ArduinoCommand(sublime_plugin.WindowCommand):
 
         args += ["--{}".format(self.action), ino]
 
+        #get start time
+        now = time.strftime("%H:%M:%S")
+        print("\n----BEGIN----"+now+"\n")
         print(args)
         try:
             a = subprocess.check_output(args, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
-            print(e.output)
+            now = time.strftime("%H:%M:%S")
+            print("\n----ERROR----"+now+"\n")
+            output = clean_output(e.output)
+            print(output)
         else:
-            print(a)
+            now = time.strftime("%H:%M:%S")
+            print("\n----DONE----"+now+"\n")
+            output = clean_output(a)
+            print(output)
 
 class ArduinoverifyCommand(ArduinoCommand):
     action = "verify"
